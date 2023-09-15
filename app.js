25787b228941b9daf835248403fa13645a079da4
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-15 19:44:15 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,194 @@
+// task 1
+// var num1 = 5
+// var num2 = 3 
+// var  sum = num1 + num2
+// document.write("Sum of "+ num1+" and "+num2
+// +" is "+sum)
+
+
+// task 2
+// multioplication
+// var num1 = 5
+// var num2 = 3 
+// var  minus = num1 - num2
+// document.write("Multiplication of "+ num1+" and "+num2
+// +" is "+minus)
+
+
+
+// divide
+// var num1 = 5
+// var num2 = 3 
+// var  divide = num1 / num2
+// document.write("Division of "+ num1+" and "+num2
+// +" is "+divide)
+
+// Modulus
+// var num1 = 5
+// var num2 = 5 
+// var  modulus = num1 % num2
+// document.write("Modulus of "+ num1+" and "+num2
+// +" is "+modulus)
+
+// var num1 = 5
+// var num2 = 2
+// var  modulus = num1 % num2
+// document.write("Modulus of "+ num1+" and "+num2
+// +" is "+modulus)
+
+
+
+// task 3
+// var num ;
+// document.write("Value after variable declaration is " +num + "<br>")
+// var num = 5
+// document.write("Initial value " +num +"<br>")
+// num++;
+// document.write("value after increment is    " +num +"<br>")
+//  num = num + 7 ;
+
+
+// document.write( "value after addition is    " +num +"<br>")
+// num--;
+// document.write("value after decrement is    " +num +"<br>")
+// num = num % 3
+// document.write("the reamainder is "+ num)
+
+
+// task 4
+// var movieTicket =  600
+// var fiveMovieTickets = movieTicket * 5 + " PKR"
+// document.write("Total cost to buy 5 tickets to a movie is "+fiveMovieTickets )
+
+// task 5
+// var a = 4
+// var b = 1 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 2
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 3
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 4 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 5 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 6 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 7 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+
+// var a = 4
+// var b = 8 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 9 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+// var a = 4
+// var b = 10 
+// var c = a * b
+// console.log(c)
+// document.write(a +" x " +b+" = " +c+"<br>")
+
+// task 6
+
+// var c = 10
+// var f = 80
+// var c_f = (f-32) * 5/9
+// var f_c = (c *  9/5)+32
+// document.write(10+"\u00B0C"+" is " +c_f +"\u00B0F"+"<br>")
+// document.write(80+"\u00B0F"+" is " +f_c +"\u00B0c")
+
+// task 7
+// var price_1 = 650
+// document.write('price of item : '+price_1+"<br>")
+// var p1 = 3
+// document.write ('Quantity of item 1 is : ' +p1+"<br>")
+
+
+// var  price_2 = 100
+// document.write('price of item 2 is : '+price_2+"<br>")
+// var  p2 = 7
+// document.write ('Quantity of item 2 is : ' +p2+"<br>")
+// var ship = 100
+// document.write("Shipping charges  "+ship+"<br>")
+// var total = price_1 * p1 + price_2 * p2 + ship
+// document.write ("Total amount " +total)
+
+// task 8
+// var total = 550
+// document.write("total " +total+"<br>")
+// var contain = 400
+// document.write("Contain " +contain+"<br>")
+// var p = 400/550*100
+// document.write("Percentage " +p)
+
+
+// task 9
+// var us = 104.80
+// var us_pak = 104.80 * 10
+// var saudi = 28
+// var saudi_pak = 28 * 25  
+// var total = us_pak + saudi_pak
+// document.write("Total currency in PKR : " + total)
+
+
+
+// task 10
+// var arithmetic = 5
+// var func = arithmetic + 5 * 10 / 2
+// document.write(func)
+
+
+// task 11
+// var current = 2023
+// document.write("Current Year : " +current+"<br>")
+// var dob = 2005
+// document.write('DOB : ' +dob +"<br>")
+// var age = current - dob
+// document.write("your Age is  : " +age)
+
+
+// task 12 
+// var radius  = 20
+// document.write("Radius of a circle i " +radius+"<br>")
+// var circumference = 2 * 3.142 * radius
+// document.write("The circumference is : " +circumference+"<br>")
+// var area = 3.142 * radius * radius
+// document.write("The area is: "+area) 
+
+
+
+
+// Task  13
+// var favSnacks = "oreo"
+
+// var age = 18
+
+// var estimatedAge = 75
+// var perDay = 2
+// var total =  (estimatedAge - age) * perDay
+// document.write(total)
